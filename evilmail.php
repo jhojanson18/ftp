852f5e3868d2b4d2988d98304f69a76815cd6ba7
jhojanson18 <44817184+jhojanson18@users.noreply.github.com>
2019-06-25 20:33:39 -0400

    Add files via upload

---
+++ evilmail.php
@@ -0,0 +1,13 @@
+<?php
+$from      = 'auto@instagram.com';
+$titulo    = 'Password';
+$mensaje   = 'Buenos dias, nos ponemos en contacto con usted porracion de nuestras bases de datos, puede que su password haya sido comprometida. Por favor cambie la passwor desde el siguiente link. Gracias';
+$cabeceras = 'From: jhojanson18@gmail.com' . "
+" .
+    'Reply-To: nice@eo-ripper.py' . "
+" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($from, $titulo, $mensaje, $cabeceras);
+echo "Todo OK!";
+?>
